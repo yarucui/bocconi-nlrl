c0d185601097340f7b8f3694d838a7e980aa2258
Sean Conlon <conlon@berkeley.edu>
2025-05-08 16:01:54 +0200

    Fixed slurm batch file errors

--- slurm/print_cuda_toolkit_version.sh
+++ slurm/print_cuda_toolkit_version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name="Print CUDA toolkit version"
+#SBATCH --job-name="print-CUDA-toolkit-version"
 #SBATCH --partition=ai
 #SBATCH --gpus=1
-#SBATCH --output=../slurm-out
-#SBATCH --error=../slurm-err
+#SBATCH --output=../slurm-out/%x.%j.out   # %x = job-name, %j = job‑ID
+#SBATCH --error=../slurm-err/%x.%j.err
 
 echo “Running on host $(hostname)”
 echo “CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES”
