09eb00fe8d6e2e7c92b6180be1750e429d3c0d5f
Sean Conlon <conlon@berkeley.edu>
2025-05-08 23:00:00 +0200

    Fixed train.sh bugs

--- slurm/train.sh
+++ slurm/train.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name="print-CUDA-toolkit-version"
+#SBATCH --job-name="train"
 #SBATCH --partition=ai
 #SBATCH --gpus=1
 #SBATCH --output=../slurm-out/%x.%j.out   # %x = job-name, %j = job‑ID
 #SBATCH --error=../slurm-err/%x.%j.err
 
+module load modules/miniconda3
+eval "$(conda shell.bash hook)"
+conda activate nlrl
+
 echo “Running on host $(hostname)”
 echo “CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES”
 
 srun python train.py configs/train.json
+
+module purge
