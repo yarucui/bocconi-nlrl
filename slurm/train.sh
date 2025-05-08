19666afca35be1895d3beaf21884cfa82bc14f41
Sean Conlon <conlon@berkeley.edu>
2025-05-08 22:51:45 +0200

    Created train slurm batch file

---
+++ slurm/train.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name="print-CUDA-toolkit-version"
+#SBATCH --partition=ai
+#SBATCH --gpus=1
+#SBATCH --output=../slurm-out/%x.%j.out   # %x = job-name, %j = job‑ID
+#SBATCH --error=../slurm-err/%x.%j.err
+
+echo “Running on host $(hostname)”
+echo “CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES”
+
+srun python train.py configs/train.json
