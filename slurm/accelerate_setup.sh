2676f5d68eaac3fc1bc038fc2906358dc007596b
Sean Conlon <conlon@berkeley.edu>
2025-05-10 21:14:33 +0200

    Added accelerate setup script

---
+++ slurm/accelerate_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name="create_env"
+#SBATCH --partition=ai
+#SBATCH --gpus=1
+#SBATCH --output=../slurm-out/%x.%j.out   # %x = job-name, %j = job‑ID
+#SBATCH --error=../slurm-err/%x.%j.err
+
+module purge
+module load nvidia/cuda-12.4.0
+module load modules/miniconda3
+
+eval "$(conda shell.bash hook)"
+
+echo “Running on host $(hostname)”
+echo “CUDA_VISIBLE_DEVICES = $CUDA_VISIBLE_DEVICES”
+
+conda activate nlrl
+
+# NOTE - Run this file using the command:
+#        sbatch --pty accelerate_setup.sh
+accelerate config
