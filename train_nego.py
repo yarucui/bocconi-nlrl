1404e5316a9fe207473e912676230aa0e1a0060b
yarucui <sisucyrcyr@gmail.com>
2025-05-15 15:36:25 +0200

    Add negotiation training script

---
+++ train_nego.py
@@ -0,0 +1,69 @@
+# External imports
+import argparse
+import json
+
+import cProfile
+import pstats
+
+# Internal imports
+from agents.actorcritic import ActorCriticAgent
+from envs.negotiation.negotiation import NegotiationEnv
+from models.mistral import Mistral
+
+if __name__ == "__main__":
+    
+    #
+    # Runtime profiler
+    #
+    profiler = cProfile.Profile()
+    profiler.enable()
+
+    #
+    # Parse CLI arguements
+    #
+    parser = argparse.ArgumentParser(prog='ActorCriticTraining')
+    parser.add_argument('train_config')
+    args = parser.parse_args()
+    #
+    # Open the train configuration file
+    #
+    with open(args.train_config, 'r') as file:
+        train_config = json.load(file)
+    #
+    # Initialize the environment
+    #
+    env = NegotiationEnv(train_config['initial_board'])
+    #
+    # Open the model configuration file and initialize the model object.
+    #
+    with open(train_config['model_config'], 'r') as file:
+        model_config = json.load(file)
+    if model_config['type'] == 'Mistral':
+        llm = Mistral(train_config['model_config'])
+    else:
+        raise ValueError(f"Unrecognized model type: {model_config['type']}")
+    #
+    # Initialize the agent
+    #
+    agent = ActorCriticAgent(env, llm, train_config['agent_config'])
+    #
+    # Run training loop
+    #
+    agent.train(
+        T=train_config['num_train_loops'], 
+        N=train_config['num_trajectories_per_loop'], 
+        K=train_config['num_sample_trajectories_per_mc']
+    )
+
+    #
+    # Runtime profiler
+    #
+    # Load the profiler into pstats
+    stats = pstats.Stats(profiler)
+    profiler.disable()
+    # Simplify file paths for readibility
+    stats.strip_dirs()
+    # Sort by 'tottime percall' (average time per call)
+    stats.sort_stats(pstats.SortKey.TIME)
+    # Print the top 20 functions
+    stats.print_stats(20)
